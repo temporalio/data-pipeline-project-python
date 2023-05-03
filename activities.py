e86faf7ece0490b0720fa7fa00ba1e0c2027fbf7
Patrick Rachford <prachford@icloud.com>
2023-05-03 13:31:01 -0700

    Updates based on Chad's comments

--- activities.py
+++ activities.py
@@ -9,48 +9,40 @@ TASK_QUEUE_NAME = "temporal-community-task-queue"
 
 
 @dataclass
-class TemporalCommunityPosts:
+class TemporalCommunityPost:
     title: str
     url: str
     views: int
-
-
-async def fetch(session: aiohttp.ClientSession, url: str) -> dict:
-    async with session.get(url) as response:
-        return await response.json()
 
 
 @activity.defn
 async def post_ids() -> List[str]:
     async with aiohttp.ClientSession() as session:
         async with session.get("https://community.temporal.io/latest.json") as response:
-            if response.status != 200:
-                raise Exception(f"Failed to fetch top stories: {response.status}")
+            if not 200 <= int(response.status) < 300:
+                raise RuntimeError(f"Status: {response.status}")
             post_ids = await response.json()
 
     return [str(topic["id"]) for topic in post_ids["topic_list"]["topics"]]
 
 
 @activity.defn
-async def top_posts(post_ids: List[Any]) -> List[TemporalCommunityPosts]:
-    results = []
+async def top_posts(post_ids: List[str]) -> List[TemporalCommunityPost]:
+    results: List[TemporalCommunityPost] = []
     async with aiohttp.ClientSession() as session:
         for item_id in post_ids:
-            try:
-                async with session.get(
-                    f"https://community.temporal.io/t/{item_id}.json"
-                ) as response:
-                    item = await response.json()
-                    slug = item["slug"]
-                    url = f"https://community.temporal.io/t/{slug}/{item_id}"
-                    community_post = TemporalCommunityPosts(
-                        title=item["title"], url=url, views=item["views"]
-                    )
-                    results.append(community_post)
-                if response.status != 200:
-                    activity.logger.error(f"Status: {response.status}")
-            except KeyError:
-                activity.logger.error(f"Error processing item {item_id}: {item}")
+            async with session.get(
+                f"https://community.temporal.io/t/{item_id}.json"
+            ) as response:
+                if response.status < 200 or response.status >= 300:
+                    print(f"Failed to fetch post {item_id}: {response.status}")
+                item = await response.json()
+                slug = item["slug"]
+                url = f"https://community.temporal.io/t/{slug}/{item_id}"
+                community_post = TemporalCommunityPost(
+                    title=item["title"], url=url, views=item["views"]
+                )
+                results.append(community_post)
     results.sort(key=lambda x: x.views, reverse=True)
     top_ten = results[:10]
     return top_ten
--- your_workflow.py
+++ your_workflow.py
@@ -3,22 +3,18 @@ from datetime import timedelta
 from typing import Any, List
 
 from temporalio import workflow
-from temporalio.common import RetryPolicy
 
 with workflow.unsafe.imports_passed_through():
-    from activities import TemporalCommunityPosts, post_ids, top_posts
+    from activities import TemporalCommunityPost, post_ids, top_posts
 
 
 @workflow.defn
 class TemporalCommunityWorkflow:
     @workflow.run
-    async def run(self) -> List[TemporalCommunityPosts]:
-        news_ids: List[str] = await workflow.execute_activity(
+    async def run(self) -> List[TemporalCommunityPost]:
+        news_ids = await workflow.execute_activity(
             post_ids,
             start_to_close_timeout=timedelta(seconds=15),
-            retry_policy=RetryPolicy(
-                non_retryable_error_types=["Exception"],
-            ),
         )
         return await workflow.execute_activity(
             top_posts,
